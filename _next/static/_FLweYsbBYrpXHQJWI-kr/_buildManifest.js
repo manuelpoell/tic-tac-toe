f8a92d97a8a4c18976b31fb0dc114a66eaef57d8
manuelpoell <manuelpoell@gmx.at>
2022-04-24 22:44:35 +0000

    Deploying to gh-pages from @ manuelpoell/tic-tac-toe@d9f9ae5abe59fcfb51c91b1c03521e7d8875b098 🚀

---
+++ _next/static/_FLweYsbBYrpXHQJWI-kr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-cd57a33b186b1adc.js","static/chunks/pages/index-f981545524f5955c.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
