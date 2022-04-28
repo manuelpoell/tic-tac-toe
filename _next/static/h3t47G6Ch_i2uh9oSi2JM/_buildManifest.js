553198e80074a0256d16856263c5b3207785add3
manuelpoell <manuelpoell@gmx.at>
2022-04-28 20:32:21 +0000

    Deploying to gh-pages from @ manuelpoell/tic-tac-toe@13a441ed1ee59e3b7006a2d4549c187837d1035c 🚀

--- _next/static/h3t47G6Ch_i2uh9oSi2JM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-cd57a33b186b1adc.js","static/chunks/pages/index-58bcf7727f40618a.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/ziXG5onL_tHhsqhnO0r_K/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/675-cd57a33b186b1adc.js","static/chunks/pages/index-d334ba50de2552ca.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
